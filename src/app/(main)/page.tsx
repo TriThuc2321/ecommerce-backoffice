a9639f793ab3c125055e47c2474345bb75e2d032
TriThuc2321 <trithuc23232@gmail.com>
2025-08-29 00:23:36 +0700

    style: setup theme

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,31 @@
+import { Button } from '@heroui/button';
+
 export default function Home() {
   return (
     <div className="h-full">
       <h5>Dashboard</h5>
+      <Button>Click me</Button>
+      <Button color="primary">Click me</Button>
+      <Button color="secondary">Click me</Button>
+      <Button color="success">Click me</Button>
+      <Button color="warning">Click me</Button>
+      <Button color="danger">Click me</Button>
+
+      <Button color="primary" variant="flat">
+        Click me
+      </Button>
+      <Button color="secondary" variant="bordered">
+        Click me
+      </Button>
+      <Button color="success" variant="solid">
+        Click me
+      </Button>
+      <Button color="warning" variant="ghost">
+        Click me
+      </Button>
+      <Button color="danger" variant="solid">
+        Click me
+      </Button>
     </div>
   );
 }

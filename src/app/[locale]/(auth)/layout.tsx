b78552013132a2851289eadaff52721fdd730fef
TriThuc2321 <trithuc23232@gmail.com>
2025-09-13 23:10:13 +0700

    feat: login page

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -7,7 +7,7 @@ type AuthLayoutProps = {
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-100">
-      <div className="bg-background w-full max-w-md rounded-3xl p-6 shadow">
+      <div className="bg-background w-full max-w-md rounded-3xl p-10 shadow">
         {children}
       </div>
     </div>
---
+++ src/configs/env.ts
@@ -0,0 +1,5 @@
+const ENV = {
+  API_URL: process.env.NEXT_PUBLIC_API_URL,
+};
+
+export default ENV;
